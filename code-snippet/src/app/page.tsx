5d88d8abe938e072082becb6dfd552240c18931b
Yash Raj <yashr3037@gmail.com>
2025-03-04 11:09:34 +0530

    code manager : hero page

---
+++ code-snippet/src/app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div>
+      <div className="flex justify-between border-red-400 border-2 p-3 rounded-2xl">
+        <h1 className="text-amber-700 font-bold text-2xl ">Home</h1>
+        <h1 className="text-red-400 font-bold text-2xl ">Code Manager</h1>
+      </div>
+      <div>
+
+      </div>
+      {/* snippet card content */}
+    </div>
+  );
+}
